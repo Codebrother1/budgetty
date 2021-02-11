8406e4521c76844523f5815abbb88e1bd79b45be
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-02-11 03:29:42 -0600

    redux lab

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -7,14 +7,20 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import { connect } from 'react-redux'
 
 
 class Budget extends Component {
 
   render() {
+  // DESTRUCTURE THE LOADING PROPERTY FROM THE BUDGET OBJECT THAT WAS MAPPED TO PROPS THROUGH MAPSTATETOPROPS/CONNECT
+    const { loading } = this.props.budget;
     return (
       <Background>
-        {true ? <Loading /> : null}
+        // UPDATE THE TERNARY TO CHECK WHETHER OR NOT THE LOADING PROPERTY 
+        // ON THE BUDGET OBJECT IS TRUE. IF SO, THEN THE LOADING COMPONENT
+        // SHOULD BE DISPLAYED
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
           <Nav />
           <div className='content-container'>
@@ -33,4 +39,14 @@ class Budget extends Component {
   }
 }
 
-export default Budget;
+// THIS FUNCTION TAKES IN THE REDUX STORE STATE AND MAPS THE BUDGET REDUCER INFO 
+// FROM THE REDUX STORE TO A BUDGET KEY ON THIS COMPONENT'S PROPS OBJECT
+function mapStateToProps(state) {
+  return {
+    budget: state.budget
+  }
+}
+
+	
+// THE CONNECT METHOD TAKES IN THE MAPSTATETOPROPS FUNCTION AND CONNECTS THIS COMPONENT TO THE REDUX STORE
+export default connect(mapStateToProps)(Budget);
